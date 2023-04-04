edeab400697eab9ca248b408dba019c1c2715a9b
Alex Njagi <1992anjagi@gmail.com>
2023-04-04 17:28:46 +0300

    Setting questions complete

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
+function generator (){
 fetch (`https://opentdb.com/api.php?amount=10&category=32&difficulty=easy&type=multiple`)
 .then (res =>res.json())
 .then ((data)=>{
-        //console.log(data);
-        //const all = (data.results[0].question)
-        console.log(data.results[0].question);
-        console.log(data.results[1].question);
-        console.log(data.results[2].question);
-        console.log(data.results[3].question);
-        console.log(data.results[4].question);
-        console.log(data.results[5].question);
+        let oneS = (data.results[0].question);                  //Variable to hold the first question
+        let twoS = (data.results[1].question);                  //Variable to hold the second question
+        let threeS = (data.results[2].question);                //Variable to hold the third question
+        let fourS = (data.results[3].question);                 //Variable to hold the fourth question
+        let fiveS = (data.results[4].question);                 //Variable to hold the fifth question
+        let sixS = (data.results[5].question);                  //Variable to hold the sixth question
+        let sevenS = (data.results[6].question);                //Variable to hold the seventh question
+        let eightS = (data.results[7].question);                //Variable to hold the eighth question
+        let nineS = (data.results[8].question);                 //Variable to hold the ninth question
+        let tenS = (data.results[9].question);                  //Variable to hold the tenth question
 
-
-
-
+        document.write(oneS);
+        document.write(twoS);
+        document.write(threeS);
+        document.write(fourS);
+        document.write(fiveS);
+        document.write(sixS);
+        document.write(sevenS);
+        document.write(eightS);
+        document.write(nineS);
+        document.write(tenS);
 })
-/*
-.then (data => console.log(data))*/
+}
+generator()
 
 
 
